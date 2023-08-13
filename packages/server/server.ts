cbc967625bbece9d06040207d0a438c991597ac2
Pat Keenan <patkeenan.dev@gmail.com>
2023-08-13 18:02:56 -0400

    feat: initial project setup

---
+++ packages/server/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const port = 3001
+
+// set the api routes to the express app
+app.use('/api', (req, res) => {
+    res.status(200).json({message: 'Hello from the API'})
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
