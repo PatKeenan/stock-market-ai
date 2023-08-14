2cb5eb7fb9eb4505f71726d767a7d0695ed5fb61
Pat Keenan <patkeenan.dev@gmail.com>
2023-08-14 09:47:38 -0400

    feat: added dev tooling

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -5,12 +5,9 @@ const port = 3001
 
 // set the api routes to the express app
 app.use('/api', (req, res) => {
-    res.status(200).json({message: 'Hello from the API'})
+    res.status(200).json({ message: 'Hello from the API' })
 })
-
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
-
